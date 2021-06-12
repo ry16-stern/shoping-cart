cffc3a9bcb2a6a4a1e287f68b2a6db733220ecda
unknown <ry16@stern.nyu.edu>
2021-06-11 23:56:00 -0400

    Added price per pound functionality. Now the program prints the header for the receipt and the list of items.Next step is to calculate and print the rest of the receipt.

--- shop.py
+++ shop.py
@@ -15,21 +15,40 @@ products_df = read_csv(csv_filepath)
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", default="OOPS, please set env var called 'SENDGRID_API_KEY'")
 SENDER_ADDRESS = os.getenv("SENDER_ADDRESS", default="OOPS, please set env var called 'SENDER_ADDRESS'")
 
+def to_usd(my_price):
+    return f"${my_price:,.2f}"
 
-def print_receipt_and_send_email(text):
+def print_receipt_and_send_email():
     STORE_NAME = os.getenv("STORE_NAME")
     STORE_WEBSITE= os.getenv("STORE_WEBSITE")
     now = datetime.now()
 
+    sp="\n---------------------------------\n"
+    nl="\n"
+    header=sp+STORE_NAME+nl+STORE_WEBSITE+sp+"CHECKOUT AT: "+now.strftime("%d-%m-%Y %H:%M %p")+sp
+    print(header)
 
-    print("---------------------------------")
-    print(STORE_NAME)
-    print(STORE_WEBSITE)
-    print("---------------------------------")
-    print(now.strftime("%d-%m-%Y %H:%M %p"))
-    print("---------------------------------")
-    print(text)
+    for item in cart:
+      price=item["price"]
+      quant=item["quant"]
+      
+      space1=" " * (50-len(item["name"]))
+      if item["price_per"]=="item":
+          tot=int(quant)*float(price)
+      else:
+          tot=item["price"]
+      line=" ... "+item["name"]+space1+"quantity: "+str(quant)+" X "+ to_usd(float(products_df.loc[products_df["id"]==int(item["id"])].to_dict(orient = 'records')[0]["price"]))+"  ("+to_usd(float(tot))+")"
+      print(line)
 
+
+    
+
+'''Main loop of the app allows user to enter an id of the item and adds an item to cart object. 
+   The loop checks if the item already in the cart and if so increments quant for items.
+   For items that are charged per pound it will ask the user to indicate the number of pounds and 
+   calculate the total amoun by multiplying pounds and price per pound. 
+   After the input is done the cart will be sent to print_receipt_and_send_email function.
+'''
 loop=1
 cart=[]
 
@@ -40,22 +59,40 @@ while loop==1:
         pick=products_df.loc[products_df["id"]==int(user_input)].to_dict(orient = 'records')[0]
         pick["quant"]=1
         loop=1
-        if(len(cart)>0):
-            match=0
-            for item in cart:
-                if item["id"]==pick["id"]:
-                    item["quant"] += 1
-                    match += 1
-                    break
-            if match<1:
-              cart.append(pick)  
-        else:
-            cart.append(pick)
-        
+        if pick["price_per"]=="item": #if an item added is counted per item
+            if(len(cart)>0):
+                match=0
+                for item in cart:
+                    if item["id"]==pick["id"]:
+                        item["quant"] += 1
+                        match += 1
+                        break
+                if match<1:
+                    cart.append(pick)  
+            else:
+                cart.append(pick)
+        else: #if an item added is counted per pound, checks if item is new or existing and calculates increments
+            pounds=input("How many pounds?")
+            price=pick["price"]*float(pounds)
+            if(len(cart)>0):
+                match=0
+                for item in cart:
+                    if item["id"]==pick["id"]:
+                        item["quant"] += float(pounds)
+                        item["price"] += float(price)
+                        match += 1
+                        break
+                if match<1:
+                    pick["quant"] = float(pounds)
+                    pick["price"] = float(price)
+                    cart.append(pick)  
+            else:
+                cart.append(pick)
+
     else:
         #exit message
         loop=0
-        print(cart)
+        print_receipt_and_send_email()
 
 
 def email():
